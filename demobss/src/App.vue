130a848ec619335770b4db40222800de638de02d
heonsoo08@nav <gjstn0308@naver.com>
2022-10-18 14:29:33 +0900

    #0 Router수정

--- demobss/src/App.vue
+++ demobss/src/App.vue
@@ -1,8 +1,40 @@
 <template>
+<<<<<<< Updated upstream
   <nav>
     <router-link to="/"></router-link>
+=======
+<nav>
+  <Header/>
+>>>>>>> Stashed changes
   </nav>
-  <router-view/>
+  <Section/>
 </template>
 
+<<<<<<< Updated upstream
+=======
+<script>
+// import { defineComponent } from '@vue/composition-api'
+// import { component } from 'vue/types/umd';
+import Header  from  './components/Header.vue'
+import Section from "./components/Section";
+
+
+export default {
+  components: {
+    Header,
+    Section,
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+>>>>>>> Stashed changes
 
--- demobss/src/pages/ContMgt.vue
+++ demobss/src/pages/ContMgt.vue
@@ -1,4 +1,36 @@
 <template>
+<div>
+<div class="c1">고객 정보 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@</div>
+
+<div class="c2"> 두번째 큰 박스
+
+  <div class="c2-1">보유 상품</div>
+
+  <div class="c2-2">청구계정 정보</div>
+
+</div>
+
+<div class="c3">계약 상세
+  <table width="100%">
+      <tr>
+        <th width="40%">계약 상세</th>
+        <th width="40%">계약 상세</th>
+      </tr>
+      <tr>
+        <th>td 3</th>
+        <td>td3 </td>
+      </tr>
+
+    </table>
+
+
+
+</div>
+<div class="c4">계약 상세
+    
+</div>
+
+</div>
 
 </template>
 
@@ -9,5 +41,42 @@ export default {
 </script>
 
 <style scoped>
+.c1 {
+  width: 800px;
+  height:200px;
+  /* position : center; */
+  background-color: aqua;
+}
+.c2 {
+  width: 800px;
+  height: 200px;
+  display: inline-block;
+  /* position : relative; */
+  /* background-color: violet; */
+}
+.c2-1 {
+  width: 100px;
+  height: 80px;
+  /* position : relative; */
+  background-color: bisque;
+}
+.c2-2 {
+  width: 100px;
+  height: 80px;
+  /* position : relative; */
+  background-color: chocolate;
+}
+.c3 {
+  width: 800px;
+  height: 200px;
+  /* position : relative; */
+  background-color: rgb(186, 192, 175);
+}
+.c4 {
+  width: 800px;
+  height: 200px;
+  /* position : relative; */
+  background-color: orange;
+}
 
 </style>
