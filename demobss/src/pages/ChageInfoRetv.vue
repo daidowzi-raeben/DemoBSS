11dca0cc793f481c7de9055977ebab60859c5cf9
yun2030 <alsghwhro@naver.com>
2022-10-18 14:27:45 +0900

    기초틀 임시본

--- demobss/src/pages/ChageInfoRetv.vue
+++ demobss/src/pages/ChageInfoRetv.vue
@@ -1,13 +1,87 @@
 <template>
+<div class="layout">
+  청구요금
+  <section>
+  <div class="cusInfo">
+    <form>
+      <label>고객조회</label> <select></select><input type="text">
+    </form>
+  </div>
+    <div class="contList" >
+      서비스계약목록</div>
+    <div style="left :30%;">
+    <div>
+      <div class="clList">
+      청구</div>
+      <div style="width:100%;">
+        <div class="feeSear" >
+        요금</div>
+        <span class="sevSear" >
+        서비스</span>
+      </div>
+    </div>
+
+    </div>
+  </section>
+</div>
 
 </template>
 
 <script>
+import Section from "@/components/Section";
 export default {
-  name: "ChageInfoRetv"
+  name: "ChageInfoRetv",
+  components: {Section}
 }
 </script>
 
 <style scoped>
-
+div.layout{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  overflow-y: scroll;
+}
+section > div.cusInfo {
+  background-color: antiquewhite;
+  width: 100%;
+  margin: 0 auto;
+  padding-bottom:10px;
+  height: 100px;
+}
+section > div.contList{
+  float: left;
+  width: 15%;
+  background-color: blue;
+  margin: 0 auto;
+  padding-bottom:10px;
+  height: 650px;
+}
+.clList{
+  width: 85%;
+  float: left;
+  background-color: blueviolet;
+  margin: 0 auto;
+  padding-bottom:10px;
+  height: 425px;
+}
+.feeSear{
+  float: left;
+  width: 30%;
+  height: 225px;
+  background-color: chartreuse;
+}
+section {
+  position: relative;
+  /* top: 177px; border: #F00 solid 1px; */
+  min-height: calc(100% - 297px);
+  padding: 40px 0 143px;
+  box-sizing: border-box;
+}
+.sevSear{
+  float: right;
+  width: 55%;
+  height: 225px;
+  background-color: cornflowerblue;
+}
 </style>
--- demobss/src/router/index.js
+++ demobss/src/router/index.js
@@ -1,20 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../pages/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'ChageInfoRetv',
+    component: () => import(/* webpackChunkName: "about" */ '../pages/ChageInfoRetv.vue')
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../pages/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
