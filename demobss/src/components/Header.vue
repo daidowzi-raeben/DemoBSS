fdf5ffc3f528b00aaad01b5618ab746fe8679f44
fjwldap <wwldrn927@naver.com>
2022-10-18 17:51:07 +0900

    header 추가

--- demobss/src/components/Header.vue
+++ demobss/src/components/Header.vue
@@ -1,13 +1,23 @@
 <template>
-
+  <header>
+    <div id="menu">
+      <div class="area">
+        <ul>
+          <li>계약</li>
+          <li>고객</li>
+          <li>청구</li>
+          <li>영업관리</li>
+          <li>정산관리</li>
+        </ul>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-  name: "Header"
-}
+  name: "Header",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
