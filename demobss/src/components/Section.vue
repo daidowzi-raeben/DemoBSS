c6afc21d5decaa530fd22b202c4a8b173f35e1d0
fjwldap <wwldrn927@naver.com>
2022-10-26 11:27:17 +0900

    동적 컴포넌트 추가..

--- demobss/src/components/Section.vue
+++ demobss/src/components/Section.vue
@@ -1,30 +1,32 @@
 <template>
   <section>
-    <Nav v-show="navOn"
-         @input="AddComponent"
-    />
+    <Nav v-show="navOn" @input="AddComponent" />
     <div class="wrap">
       <div class="menu_tab_line">
         <div v-for="(item, index) in compm" v-bind:key="index">
           <div
             :class="{
-              tabon: this.comp === item.cmpnId,
-              taboff: this.comp !== item.cmpnId,
+              tabon: this.comp === item.menuId,
+              taboff: this.comp !== item.menuId,
               menu_tab: true,
             }"
-            @click="ChageComponent(item.cmpnId, index)"
+            @click="ChageComponent(item.menuId, index)"
           >
-            {{ item.menuNm }}
-            <span class="tab_x" @click.prevent.stop="DeleteComponent(index)">x</span>
+            <span id="tab_nm">
+              {{ item.menuNm }}
+            </span>
+            <span class="tab_x" @click.prevent.stop="DeleteComponent(index)"
+              >x</span
+            >
           </div>
         </div>
 
         <div class="menu_close">x</div>
       </div>
 
-      <title-area :menuNm="menuNm" />
-      <keep-alive class="view_wrap" :max="10" >
-        <component v-bind:is="comp"></component>
+      <title-area :currentMenu="currentMenu" />
+      <keep-alive class="view_wrap" :max="10">
+        <component v-bind:is="component"></component>
       </keep-alive>
     </div>
   </section>
@@ -33,37 +35,64 @@
 <script>
 import TitleArea from "./common/TitleArea.vue";
 import Nav from "./Nav.vue";
-import ChageInfoRetv from "../pages/ChageInfoRetv.vue";
-import ContMgt from "../pages/ContMgt.vue";
-import dummy from "../pages/dummy.vue";
-import CommonView from "../pages/CommonView.vue";
+// import ChageInfoRetv from "../pages/ChageInfoRetv.vue";
+// import ContMgt from "../pages/ContMgt.vue";
+// import dummy from "../pages/dummy.vue";
+// import CommonView from "../pages/CommonView.vue";
+import { defineAsyncComponent } from "vue";
+
 export default {
   name: "Section",
-  components: { TitleArea, Nav, ChageInfoRetv, ContMgt, dummy, CommonView },
+  components: {
+    TitleArea,
+    Nav,
+    // , ChageInfoRetv, ContMgt, dummy, CommonView
+  },
   data() {
     return {
-      comp: "ChageInfoRetv",
+      comp: "bill_01_01_01",
       cur_num: 0,
       compm: [
-        { cmpnId: "ChageInfoRetv", menuNm: "청구관리" },
-        { cmpnId: "ContMgt", menuNm: "계약관리" },
-        { cmpnId: "CommonView", menuNm: "공통" },
+        {
+          cmpnId: "ChageInfoRetv",
+          menuId: "bill_01_01_01",
+          menuNm: "청구요금정보조회",
+          upMenuId: "bill_01_01",
+        },
+        // {
+        //   cmpnId: "ContMgt",
+        //   menuId: "cont_01_01_02",
+        //   menuNm: "계약관리",
+        //   upMenuId: "cont_01_01",
+        // },
+        // {
+        //   cmpnId: "CommonView",
+        //   menuId: "comn_01_01_01",
+        //   menuNm: "공통화면참고",
+        //   upMenuId: "comn_01_01",
+        // },
       ],
-      compm2:[],
+      compm2: [],
       test: "ChageInfoRetv",
+      component: defineAsyncComponent(() =>
+        import("../pages/ChageInfoRetv.vue")
+      ),
     };
   },
   watch: {
     cur_num: function (newVal, oldVal) {
-      this.comp = this.compm[newVal].cmpnId;
+      this.comp = this.compm[newVal].menuId;
+      this.component = defineAsyncComponent(() =>
+        import("../pages/" + this.compm[newVal].cmpnId + ".vue")
+      );
     },
   },
   computed: {
     navOn() {
       return this.$store.state.navOn;
     },
-    menuNm() {
-      return this.compm[this.cur_num].menuNm;
+    currentMenu() {
+      return this.compm[this.cur_num];
     },
   },
   methods: {
@@ -74,20 +103,27 @@ export default {
     DeleteComponent: function (index) {
       if (index != 0) {
         this.compm.splice(index, 1);
-        if (index == this.cur_num || (index < this.cur_num && index <= this.compm.length)) {
+        if (
+          index == this.cur_num ||
+          (index < this.cur_num && index <= this.compm.length)
+        ) {
           this.cur_num = this.cur_num - 1;
         }
       }
     },
     AddComponent: function (param) {
-      if(param.cmpnId!="" && param.cmpnId != null) {
-        const st = this.compm.find(element => element.cmpnId === param.cmpnId);
+      console.log(param);
+      if (param.menuId != "" && param.menuId != null) {
+        const st = this.compm.find(
+          (element) => element.menuId === param.menuId
+        );
+        console.log(st);
         if (st != null) {
           var i = this.compm.indexOf(st);
           this.cur_num = i;
         } else {
           if (this.compm.length <= 10) {
-            this.compm.push({cmpnId: param.cmpnId, menuNm: param.menuNm});
+            this.compm.push(param);
             this.cur_num = this.compm.length - 1;
           } else {
             console.log("10개를 넘었습니다.");
--- demobss/src/components/common/TitleArea.vue
+++ demobss/src/components/common/TitleArea.vue
@@ -1,29 +1,59 @@
 <template>
   <div class="title_area">
     <div class="icon"></div>
-    <h1>메뉴 > 메뉴2 ></h1>
-    <span> {{ menuNm }} [BILL_01_001]</span>
+    <h1>
+      {{ menuDepth1.menuNm }} > {{ menuDepth2.menuNm }} >
+      <span> {{ currentMenu.menuNm }} [{{ currentMenu.menuId }}]</span>
+    </h1>
     <div class="right">
-      <button-component :btnClass="'btnClass1'" :btnName="'홈화면설정'"></button-component>
-      <button-component :btnClass="'btnClass1'" :btnName="'화면초기화'" ></button-component>
+      <button-component
+        :btnClass="'btnClass1'"
+        :btnName="'홈화면설정'"
+      ></button-component>
+      <button-component
+        :btnClass="'btnClass1'"
+        :btnName="'화면초기화'"
+      ></button-component>
     </div>
   </div>
 </template>
 
 <script>
 import ButtonComponent from "./ButtonComponent.vue";
+import menu from "../../../public/menu.json";
 
-export default{
-  components: { ButtonComponent }, 
+export default {
+  components: { ButtonComponent },
   name: "TitleArea",
   data() {
-    return {};
+    return {
+      menuDepth1: {},
+      menuDepth2: {},
+    };
   },
   props: {
-    menuNm: String,
+    currentMenu: Object,
   },
-  computed: {},
-  created() {},
+  created() {
+    this.menuDepth2 = menu.menu.filter((menu) => {
+      return menu.menuId == this.currentMenu.upMenuId;
+    })[0];
+
+    this.menuDepth1 = menu.menu.filter((menu) => {
+      return menu.menuId == this.menuDepth2.upMenuId;
+    })[0];
+  },
+  watch: {
+    currentMenu() {
+      this.menuDepth2 = menu.menu.filter((menu) => {
+        return menu.menuId == this.currentMenu.upMenuId;
+      })[0];
+
+      this.menuDepth1 = menu.menu.filter((menu) => {
+        return menu.menuId == this.menuDepth2.upMenuId;
+      })[0];
+    },
+  },
   methods: {},
 };
 </script>
