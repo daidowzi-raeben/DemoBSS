b6ebfbdc417dcd95fa84fccc2eec2bc2713678fa
heonsoo08@nav <gjstn0308@naver.com>
2022-11-04 14:39:41 +0900

    #0 리팩토링 - App.vue 클렌징
    사용하지 않는 연결 된 컴포넌트 및 함수 제거

--- demobss/src/App.vue
+++ demobss/src/App.vue
@@ -7,7 +7,6 @@
 
 <script>
 import Header from "./components/Header.vue";
-import Menu from "./components/Menu";
 import Section from "./components/Section.vue";
 
 export default {
@@ -15,19 +14,6 @@ export default {
   components: {
     Header,
     Section,
-    Menu,
-  },
-  data() {
-    return {
-      check : 0,
-
-    }
-  },
-  methods :{
-    check_num(){
-      if(this.check==0)this.check=1;
-      else this.check=0;
-    }
   }
 };
 </script>
