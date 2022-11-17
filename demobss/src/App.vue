923db84b143565c9dff87c79e26d53749dce05cc
heonsoo08@nav <gjstn0308@naver.com>
2022-11-17 18:01:21 +0900

    #1117 App.vue 로그인화면 연동
    - url이 login이면 app.vue에서 login 컴포넌트 출력하도록 v-if 사용
    - router에 컴포넌트 추가
    - login화면에 필요한 이미지 추가 및 수정, 삭제

--- demobss/src/App.vue
+++ demobss/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="wrap">
+  <div v-if="this.$route.path =='/login'" id="Login">
+    <login />
+  </div>
+  <div v-else id="wrap">
     <Header />
     <Section />
   </div>
@@ -8,12 +11,14 @@
 <script>
 import Header from "./components/Header.vue";
 import Section from "./components/Section.vue";
+import Login from './pages/Login/Login.vue';
 
 export default {
   name: "App",
-  components: {
+  components: { 
     Header,
     Section,
+    Login
   }
 };
 </script>
--- demobss/src/router/index.js
+++ demobss/src/router/index.js
@@ -2,13 +2,19 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
+    path: "/Login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../pages/Login/Login.vue"),
+  },
+  {
     path: "/ContSection",
     name: "ContSection",
     component: () =>
       import(/* webpackChunkName: "about" */ "../pages/ContSection.vue"),
   },
   {
-    path: "/",
+    path: "/ChageInfoRetv",
     name: "ChageInfoRetv",
     component: () =>
       import(/* webpackChunkName: "about" */ "../pages/ChageInfoRetv.vue"),
