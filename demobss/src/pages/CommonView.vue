c5924984bb8a3df10db66bfd65e14bb946c5f83a
heonsoo08@nav <gjstn0308@naver.com>
2022-11-22 14:40:49 +0900

    #1122 주소 검색 팝업창 (1) 공통화면, 주소검색팝업컴포넌트,
    [CommonView.vue]
    - 주소 검색 input 혹은 button 클릭 시 팝업 창 생성
    - PostCodeComponent에서 주소 선택 시, 주소 값 중 우편번호, 도로명주소 , 지번주소 , 상세주소(입력 시에) 데이터 emit 으로 데이터 바인딩하여 출력
    - postCodeEx.json에서 각 파라미터 변수명 참고
    
    [PostCodeComponent]
    - formdatapopupcomponent 틀에 맞춰 주소 검색 팝업 구현
    - 검색 버튼 안눌렀을 경우 테이블 no show  (해당 변수 데이터 있는지 v-if)
    - 검색 버튼 클릭 시, json에서 데이터 get , 테이블 show
    - 상세주소 입력 시, input 박스에서 v-model로 데이터 바인딩 시킴.
    - 선택 버튼 클릭 시, 위에서 입력한 상세주소 포함하여 해당 주소 emit으로 상위 컴포넌트에 전달하며 팝업(모달)창 닫음
    
    [PostCodeEx.json]
    - 신사동 dummy 데이터 5개
    
    [To Do]
    1. 더미데이터 개수 증가시키고 "페이징" 기능 추가 해야함.

--- demobss/src/pages/CommonView.vue
+++ demobss/src/pages/CommonView.vue
@@ -282,6 +282,58 @@
     <br /><br /><br />
 
 
+    <h1 style="font-size: 30px">PostCodeComponent</h1>
+    <span></span>
+    <div class="commondiv1">
+      <div>
+        <table >
+
+          <tr>
+            <th style="border:1px solid black;"> <h1> &nbsp;우편번호 </h1> </th>
+            <td> {{ postCodeObj.zipNo }} </td>
+          </tr>
+          <tr>
+            <th style="border:1px solid black;"> <h1> 도로명주소</h1> </th>
+            <td> {{ postCodeObj.roadAddr }} </td>
+          </tr>
+          <tr>
+            <th style="border:1px solid black;"> <h1> &nbsp;지번주소  </h1> </th>
+            <td> {{ postCodeObj.jibunAddr }} </td>
+          </tr>
+          <tr>
+            <th style="border:1px solid black;"> <h1> &nbsp;상세주소</h1> </th>
+            <td> {{ postCodeObj.detailPostAddress }} </td>
+          </tr>
+          <!-- <h1>우편번호  : <span>  {{ postCodeObj.zipNo }}           </span></h1>
+          <h1>도로명주소: <span>  {{ postCodeObj.roadAddr }}        </span></h1>
+          <h1>지번주소  : <span>  {{ postCodeObj.jibunAddr }}       </span></h1>
+          <h1>상세주소  : <span>  {{postCodeObj.detailPostAddress}} </span></h1> -->
+        </table>
+      </div>
+      </div><div>
+      <span> 주소 검색  </span>
+      <input-component
+      :inputClass="'class1'"
+      :width="'210'"
+      :height="'25'"
+      :placeholder="'주소를 입력해 주세요. (클릭 시 주소 팝업)'"
+      @click="postCodePopup"
+      />
+      <button @click="postCodePopup">주소출력 PopUp </button>
+
+      <PostCodeComponent
+      ref="PostCodeComponent"
+      v-if="isPostCodeModalShow"
+      @FormPopup="isPostCodeModalShow = false"
+      @AGREE = "''"
+      :PopupTitleMsg="'주소 검색'"
+      :formDataPopupFrameWidth="'800px'"
+      :reqtype="'searchPostCode'"
+      @selected-juso-data="selectedJuso"/>
+      <br /><br /><br />
+    </div>
+
+
 
     <h1 style="font-size: 30px">ChkBoxComponent</h1>
     <div class="commondiv1" >
@@ -526,9 +578,12 @@ import linkComponent from '@/components/common/linkComponent.vue';
 import AtcRegComponent from "@/components/common/AtcRegComponent";
 import AtcListComponent from "@/components/common/AtcListComponent";
 import TextAreaComponent from "@/components/common/TextAreaComponent";
+import PostCodeComponent from '@/components/common/PostCodeComponent.vue';
+
 export default {
   name: "CommonView",
   components: {
+    PostCodeComponent,
     TextAreaComponent,
     AtcListComponent,
     AtcRegComponent,
@@ -690,8 +745,10 @@ export default {
       isModalShow: false, // popup 조건
       isFormModalShow: false, // Form Data popup 조건
       isFormModalShow2: false, // Form Data popup 조건
+      isPostCodeModalShow : false,
       OutputFormData : [],
       SubmitFormData : [],
+      postCodeObj : {},
     };
   },
   created() {
@@ -715,6 +772,17 @@ export default {
       else this.isFormModalShow2 = false
       this.$refs.form-data-popup-component.GetSubmitFormData()
     },
+    postCodePopup(){
+      if (this.isPostCodeModalShow == false) this.isPostCodeModalShow = true
+      else this.isPostCodeModalShow = false
+      // this.$refs.postCodePopup.GetSubmitFormData()
+    },
+    selectedJuso(postCodeData,detailPostAddress){
+      this.postCodeObj = postCodeData;
+      this.postCodeObj["detailPostAddress"] = detailPostAddress
+      this.isPostCodeModalShow = false;
+
+    }
   },
 };
 </script>
