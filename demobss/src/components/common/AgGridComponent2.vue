c3aa3bce55ff63d372abb37904f40d90cb0d16ab
heonsooo <gjstn0308@naver.com>
2022-12-07 17:07:38 +0900

    12.07 #2 ag-grid2 컴포넌트 ( 청구리스트에 사용 ) + ag-grid 속성 참고
    [ag-grid-vue의 속성]
    :heavy_check_mark:  :suppressMovableColumns="true" :arrow_forward: 헤더 컬럼 위치 이동 해제
    :heavy_check_mark: :suppressRowHoverHighlight="true" arrow_forward:  row에 먹이는 css 적용 허락해주는 속성(?)
    
    [ag-grid-vue css클래스]
    .ag-header-cell : 헤더 셀 속성 적용 ( 색상, border 적용함)
    .ag-header-cell-label : 헤더 컬럼 가운데 정렬에 사용
    :star: ag-theme-alpine 해당 클래스 내에 --ag ~~~ 이 속성들 이용해서 다양한 css 적용 가능한 것으로 보임
         해당 코드에서는 seleted row background-color 적용
    
    [To Do]
    추가적으로  ag-grid border-top 부분 색 지우고 싶음 !!

--- demobss/src/components/common/AgGridComponent2.vue
+++ demobss/src/components/common/AgGridComponent2.vue
@@ -6,10 +6,12 @@
   :rowData="rowData"
   :rowHeight="30"
   :suppressRowTransform="true"
+  :suppressMovableColumns="true"
   rowSelection="multiple"
   @grid-ready="onGridReady"
   :pagination="true"
   :paginationPageSize="paginationPageSize"
+  :suppressRowHoverHighlight="true"
   ></ag-grid-vue>
 </template>
 
@@ -19,108 +21,46 @@ import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import { AgGridVue } from 'ag-grid-vue3';
 
 export default {
+  components:{},
+  props:{
+    rowData: Object
+  },
   data(){
     return{
       paginationPageSize: null,
       servState :["사용중","예약(기설중)","정지","해지"],
       columnDefs:[
-    { headerName: "순서",           field: "index",       width:100,  checkbox:true ,sortable:true, rowSpan: params => params.data.index != null ? 2 : 1, cellClass: 'cell-span'},
-    { headerName: "서비스계약ID",   field: "ServID" ,     width:130,   sortable:true, rowSpan: params => params.data.ServID != null ? 2 : 1, cellClass: 'cell-span'},
-    { headerName: "고객ID",         field: "CustID",      width:130, rowSpan: params => params.data.CustID != null ? 2 : 1, cellClass: 'cell-span'},
-    { headerName: "청구계정ID",     field: "SubsID" ,     width:130,  rowSpan: params => params.data.SubsID != null ? 2 : 1, cellClass: 'cell-span'},
-    { headerName: "고객명",         field: "CustName" ,   width:80,  rowSpan: params => params.data.CustName != null ? 2 : 1, cellClass: 'cell-span'},
-    { headerName: "상품명",         field: "ProductName", width:415, 
-    rowSpan: function(params) {
-        return params.data.ProductName === '[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션' ? 2 : 1; },
-        cellClassRules: { 'cell-span': "true", },
+        { headerName: "순서",           field: "index",       width:100,  cellClass: 'cell-span'},
+        { headerName: "서비스계약ID",   field: "ServID" ,     width:130,  cellClass: '"cell"-span'},
+        { headerName: "고객ID",         field: "CustID",      width:130,  cellClass: 'cell-span'},
+        { headerName: "청구계정ID",     field: "SubsID" ,     width:130,  cellClass: 'cell-span'},
+        { headerName: "고객명",         field: "CustName" ,   width:80,   cellClass: 'cell-span'},
+        { headerName: "상품명",         field: "ProductName", width:428,  cellClassRules: { 'cell-span': "true", }},
+        // rowSpan: function(params) {
+        //     return params.data."ProductName" === '[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션' ? 2 : 1; },
+        { headerName: "서비스계약 상태", field: "ServState",   width:140,  
+          // colSpan: params => !this.servState.includes(params.data.ServState) ? 4 : 1,
         },
-        
-    { headerName: "서비스계약 상태", field: "ServState",   width:140, filter:true, colSpan: params => !this.servState.includes(params.data.ServState) ? 4 : 1 },
-    { headerName: "청약상태",       field: "SubsState" ,   width:90   },
-    { headerName: "개통일시",       field: "StartDate" ,   width:165, sortable:true},
-    { headerName: "정지/해지일시",   field: "EndDate" ,    width:165, sortable:true},  
-    ],
-      rowData: null,
-    }},
+        { headerName: "청약상태",       field: "SubsState" ,   width:90 },
+        { headerName: "개통일시",       field: "StartDate" ,   width:165},
+        { headerName: "정지/해지일시",   field: "EndDate" ,    width:165},  
+      ],
+    }
+  },
   components:{
     AgGridVue
   },
   methods:{
     onGridReady(params){
-      console.log("hi",this.rowData)
     },
-
   },
   created(){
-    this.paginationPageSize = 10*2;
-  },
-  mounted(){
-    this.rowData=[
-    { index: 1, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션", ServState: "정지", SubsState: "청약완료",   StartDate: "2022-12-11 10:26:30", EndDate: "2023-12-31 00:00:00"},
-    {ServState: "abcd"},
-    
-    
-    { index: 2, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-12 10:30:30", EndDate: "2023-12-31 00:00:00"},
-    {ServState: "abcd"},
-    
-    { index: 3, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-13 10:26:30", EndDate: "2023-12-31 00:00:00"},
-    {ServState: "abcd"},
-    
-    { index: 4, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "예약(기설중)", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-15 10:26:30", EndDate: "2023-12-31 00:00:00"},
-    {ServState: "abcd"},
-    
-    { index: 5, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "예약(기설중)", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-17 10:26:30", EndDate: "2023-12-31 00:00:00"},
-    {ServState: "abcd"},
-    
-    { index: 6, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "예약(기설중)", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-18 10:26:30", EndDate: "2023-12-31 00:00:00"},
-    {ServState: "abcd"},
-    
-    { index: 7, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-19 10:26:30", EndDate: "2023-12-31 00:00:00"},
-    {ServState: "abcd"},
-    
-    { index: 8, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-20 10:26:30", EndDate: "2023-12-31 00:00:00"},
-    {ServState: "abcd"},
-    
-    { index: 9, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 10, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 30, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 31, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 32, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 33, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 34, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 49, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 59, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 69, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 79, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-01 10:26:30", EndDate: "2023-12-31 00:00:00"}, {ServState: "abcd"},
-    { index: 89, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "해지", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-10-01 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 11, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "해지", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-10-01 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 12, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-08-11 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 13, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-06-21 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 14, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "정지", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-08-21 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 15, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "정지", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-11-31 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 16, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-10-01 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 17, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-03 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 18, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "해지", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-04 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 19, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-11-06 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 20, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-05 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 21, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "해지", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-12-09 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 22, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "해지", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-11-21 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 23, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-01-21 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 24, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-02-11 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 25, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "예약(기설중)", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-05-11 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 26, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "예약(기설중)", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-10-31 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 27, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "예약(기설중)", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-08-21 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 28, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-07-11 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"},
-    { index: 29, ServID: "S000008950", CustID: "C000053216", SubsID: "P000021352",  CustName: "홍길동", ServState: "사용중", SubsState: "청약완료",  ProductName : "[IDC]kt cloud SSD 컴퓨팅 스토리지 서버리스 애플리케이션",  StartDate: "2022-06-11 10:26:30", EndDate: "2023-12-31 00:00:00"},{ServState: "abcd"}
-    ]; 
+    this.paginationPageSize = 7*2;
   }
 }
 </script>
 
 <style >
-tbody:hover{
-  background-color: black;
-}
 .agtable {
   border : 1px solid black;
 }
@@ -144,10 +84,27 @@ tbody:hover{
 
 .cell-span {
   /* background-color: rgb(231,231,231); */
-  background-color: white;
+  /* background-color: white; */
 }
-.ag-cell{
-    border: 1px dotted black;
+.ag-header-cell{
+  background-color: rgb(239, 245, 252);
+  border: .5px solid rgb(231,231,231);
+}
+.ag-header-cell-label {
+  /* 컬럼 헤더 가운데 정렬 */
+  justify-content: center;
+}
+.ag-row{
+  background-color: rgb(255, 254,238);
+}
+.ag-row:hover{
+  background-color: rgb(255, 254,238);
+}
+.ag-theme-alpine{
+  --ag-selected-row-background-color:rgb(255, 254,238);
+}
+.ag-grid_sp{
+  border: none;
 }
 .ag-row .ag-cell {
   display: flex;
