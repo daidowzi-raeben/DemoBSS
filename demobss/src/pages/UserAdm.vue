b1ce13ccd8efe3b3416b5f25fc3ea85e789e31ee
heonsooo <gjstn0308@naver.com>
2022-12-09 09:38:29 +0900

    12.08 #3 사용자관리 페이지 개발 (1)
    [UserAdm.vue]
    - 사용자관리 페이지 생성 및 grid를 이용한 레이아웃
    - 사용자 검색 컴포넌트 생성 및 호출
    
    [UserAdmRetvComponent.vue]
    - 사용자 검색 컴포넌트 생성 및 table 이용한 레이아웃 (colspan 활용)
    
    [Issue]
    UI설계서에서 사용자 검색 컴포넌트내 셀렉트박스 설정에 대한 요구사항 및 현재 상황 이슈
    [요구사항]
    - 셀렉트박스의 placeholder로 각 선택 박스에 대한 안내 메시지 전달이 필요하며
      이후 클릭 시, 해당 placeholder는 셀렉트 박스 내 선택사항에 없어야 한다.
     [공통업무 - 사용자관리- 선택조건 - 페이지 31]
    
    [현재상황]
    - 기존에는 select box 클릭하지 않았을 시에, default value로 선언한 값을 props로 받아 표시했고
    이를 셀렉트박스 컴포넌트에서 값으로 선택할 수 있었다.
    
    [개선사항]
    - 셀렉트박스의 속성에 placeholder 기능이 존재하지 않기 때문에,
    select box 컴포넌트에 disabled속성을 사용하기 위해 isDisabled라는 props를 추가했음.
    props로 받은 isDisabled값을 이용해  disabled, hidden 처리하여
    셀렉트박스에서 placeholder 한 디폴트 값 이후 클릭에 대해서는 해당 값 선택(disabled)하지도 , 보여지지도 않도록 (hidden) 개발함
    (default값은 처음에 placeholder 처럼 보이기만 하고 이후에는 선택 값 없도록 하기 위한 로직)

---
+++ demobss/src/pages/UserAdm.vue
@@ -0,0 +1,53 @@
+<template>
+<article class="UserAdmContainer">
+  <div class="item1">
+    <user-adm-retv-component />
+  </div>
+  <div class="item2">
+  </div>
+
+  <div class="item3">
+    </div>
+  </div>
+</article>
+
+
+</template>
+
+<script>
+import UserAdmRetvComponent from '@/components/common/UserAdmRetvComponent.vue'
+export default {
+  mixins:[ApiMixin],
+  components: { UserAdmRetvComponent, AgGridComponent2, SubInfoTitle, ButtonComponent, SelectBoxComponent, PagingArea, LabelComponent, InputComponent },
+  data(){
+    return{
+    }
+  },
+  methods:{
+  }
+
+}
+</script>
+
+<style scoped>
+.UserAdmContainer{
+  display: grid;
+  grid-template-columns: 850px 700px 1fr;
+  grid-template-rows: 100px 640px 1fr;
+  gap: 20px 30px;
+}
+
+.UserAdmContainer > .item1{
+  grid-row: 1;
+  grid-column: 1/3;
+  background-color: burlywood;
+}
+.UserAdmContainer > .item2{
+  grid-row: 2;
+  grid-column: 1;
+}
+.UserAdmContainer > .item3{
+  grid-row: 2;
+  grid-column: 2;
+}
+</style>
