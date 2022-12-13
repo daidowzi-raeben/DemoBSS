be2958cbe8403996269f8dfa18022f3f0dc55d80
heonsooo <gjstn0308@naver.com>
2022-12-13 17:35:32 +0900

    test CORS Policy
    12.13 #1
    
    :heavy_check_mark: 크로스 오리진 정책 해결 테스트
    1) .axios 설정에서 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'; 접근 제어 오리진 허락에 모든 것 허용 방법
    2) vue.config.js 내에 devServer -  proxy 설정으로 우회하도록
    
    :heavy_check_mark: 이를 테스트하기 위한 json-server에 json파일 복사

--- demobss/src/main.js
+++ demobss/src/main.js
@@ -6,7 +6,14 @@ import axios from "axios";
 import VueAxios from "vue-axios";
 import VirtualList from "vue3-virtual-scroll-list";
 
-axios.defaults.baseURL = "http://localhost:8080/";
+
+// cors 이슈에 대응하는 방법 1) 
+// 참고 : https://rkaehdaos.github.io/dev/frontend/Vue-js/vue-use-proxy/
+axios.defaults.baseURL = "http://localhost:8081";
+// axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 
 createApp(App)
   .use(store)
--- demobss/vue.config.js
+++ demobss/vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 const { ESLint } = require('eslint')
 module.exports = defineConfig({
+  // cors 이슈에 대응하는 방법 2)
+  // 참고 :  https://jasunhee.tistory.com/312
+  devServer: {
+    proxy: 'http://localhost:8080'
+  },
   transpileDependencies: true,
   lintOnSave : false,
 })
