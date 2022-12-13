20a4706ad23670c4128b825b1312f85213065a85
yoon2030 <alsghwhro@naver.com>
2022-12-13 14:36:13 +0900

    #1213 CORS 관련 설정
    - 뷰 포트 : 8081 , 서버 포트 : 8080
    - 프록시서버 설정

--- demobss/src/main.js
+++ demobss/src/main.js
@@ -6,6 +6,6 @@ import axios from "axios";
 import VueAxios from 'vue-axios'
 import VirtualList from 'vue3-virtual-scroll-list'
 
-axios.defaults.baseURL = 'http://localhost:8080/'
+// axios.defaults.baseURL = 'http://localhost:8080/'
 
 createApp(App).use(store).use(router).use(VueAxios, axios).use('virtual-list',VirtualList).mount('#app')
--- demobss/vue.config.js
+++ demobss/vue.config.js
@@ -3,4 +3,13 @@ const { ESLint } = require('eslint')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave : false,
+  devServer:{
+    proxy: {
+      '/': {
+        "target": 'http://localhost:8080',
+        "pathRewrite" :{'^/':''},
+        "changeOrigin": true
+      },
+    }
+  }
 })
