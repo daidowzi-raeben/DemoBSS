1ed4287ece9878ab0bf0d57ecc189e48f94a04d0
yoon2030 <alsghwhro@naver.com>
2022-12-16 14:25:13 +0900

    #1216 휴일관리 페이지 구현
    - 휴일 관련 페이지의 Ag Grid CellRenderer를 위해
       HoliDesc : 휴일 설명(휴일을 경우 글자색 빨간색)
       HoliDiv : 휴일 구분
     추가 생성

---
+++ demobss/src/pages/HoliAdm.vue
@@ -0,0 +1,348 @@
+<template>
+  <article class="HoliAdmView">
+    <div class="item"> <!--1번 -->
+      <div class="holiSearch">
+        <table>
+          <tr>
+            <th><label-component :labelNm="'조회년월'" /></th>
+            <td>
+             <date-picker-component
+                 :classWrapper="'calender_input'"
+                 :width="'200px'"
+                 :height="'28px'"
+                 :pPlaceholder="'2022.01.01'"
+                 :pDate="holiStDate"
+                 @input="
+              (value) => {
+                holiStDate = value;
+              }
+            "
+               />
+            </td>
+            <td>~</td>
+            <td>
+              <date-picker-component
+                  :classWrapper="'calender_input'"
+                  :width="'200px'"
+                  :height="'28px'"
+                  :pPlaceholder="'2022.01.01'"
+                  :pDate="holiEnDate"
+                  @input="
+              (value) => {
+                holiEnDate = value;
+              }
+            "
+              />
+            </td>
+            <td class="emptyBox1"></td>
+            <th><label-component :labelNm="'휴일여부'" /></th>
+            <td>
+              <select-box-component
+                  style="height: 28px"
+                  :selectClass="'select_input4'"
+                  :width="200"
+                  :cdGroup="'holiYn'"
+                  :defaultValue="'휴일여부 선택'"
+                  :isDisabled="true"
+                  :selectedValue="holiYn"
+                  @input="(value) => {holiYn = value;}"
+              />
+            </td>
+            <td class="emptyBox2"></td>
+
+            <td class="btnBox">
+              <button-component
+                  :btn-class="'btnClass4'"
+                  :btnFontWeight="'bold'"
+                  :btn-name="'검색'"
+                  :btnHeight="'28px'"
+                  :btnWidth="'78px'"
+                  @click="search"
+              />
+            </td>
+          </tr>
+
+          <tr>
+            <th><label-component :labelNm="'휴일구분'" /></th>
+            <td>
+              <select-box-component
+                  style="height: 28px"
+                  :selectClass="'select_input4'"
+                  :width="200"
+                  :cdGroup="'holiDiv'"
+                  :defaultValue="'휴일구분 선택'"
+                  :isDisabled="true"
+                  :selectedValue="holiDiv"
+                  @input="(value) => {holiDiv = value;}"
+              />
+            </td>
+
+            <td colspan="6"></td>
+            <td>
+              <button-component
+                  :btnClass="'btnClass5'"
+                  :btnHeight="'28px'"
+                  :btnWidth="'78px'"
+                  :btnFontWeight="'bold'"
+                  :btnName="'초기화'"
+                  @click="reset"
+              />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div class="item"> <!--2번 -->
+      <div style="width: 100%">
+        <subInfoTitle :subInfoTitleNm="'휴일 리스트'"/>
+        <p style="margin-left:5px; display:inline-block;">(총 <label style="font-weight: bold">{{ total }}</label>건)</p>
+
+        <span style="float: right">
+      <buttonComponent
+          :btnClass="'btnClass3'"
+          :btnName="'엑셀다운로드'"
+          :btnWidth="'auto'"
+      />
+        </span>
+        <span style="float: right">
+      <buttonComponent
+          :btnClass="'btnClass4'"
+          :btnName="'변경'"
+          :btnWidth="'100px'"
+          @click="isModalChgShow=true"
+      />
+        </span>
+      </div>
+      <div class="ag-grid_sp">
+        <ag-grid-component
+            :header-color="'rgb(239 245 252)'"
+            :rowData="holiRowData"
+            :columnDefs="holiColumnDefs"
+            :row-height="40"
+            :isWidthFit="false"
+            :overlayNoRowsTemplate="
+          `<span> <br>` + '<br />조회 결과가 없습니다.' + ` </span>`
+          "
+        />
+      </div>
+    </div>
+    <div class="item"> <!--3번 -->
+      <div style="width: 100%">
+        <subInfoTitle :subInfoTitleNm="'달력생성'"/>
+
+        <span style="float: right">
+      <buttonComponent
+          :btnClass="'btnClass4'"
+          :btnName="'달력생성'"
+          :btnWidth="'100px'"
+          @click="isModalCretShow=true"
+      />
+        </span>
+      </div>
+      <table class="cretTable">
+        <tr>
+        <th><label-component :labelNm="'조회년월'" /></th>
+        <td>
+          <date-picker-component
+              :classWrapper="'calender_input'"
+              :width="100"
+              :pPlaceholder="'2022.01.01'"
+              :pDate="date1"
+              @input="
+              (value) => {
+                date1 = value;
+              }
+            "
+          />
+        </td>
+        <td>~</td>
+        <td>
+          <date-picker-component
+              :classWrapper="'calender_input'"
+              :width="100"
+              :pPlaceholder="'2022.01.01'"
+              :pDate="date1"
+              @input="
+              (value) => {
+                date1 = value;
+              }
+            "
+          />
+        </td>
+        </tr>
+      </table>
+    </div>
+    <popup-component
+        v-if="isModalChgShow"
+        @popup="isModalChgShow = false"
+        @AGREE = "''"
+        :popupmsg="'변경하시겠습니까?'"
+    />
+    <popup-component
+        v-if="isModalCretShow"
+        @popup="isModalCretShow = false"
+        @AGREE = "''"
+        :popupmsg="'생성하시겠습니까?'"
+    />
+  </article>
+</template>
+
+<script>
+import agGridComponent from "@/components/common/AgGridComponent";
+import datePickerComponent from "@/components/common/DatePickerComponent";
+import selectBoxComponent from "@/components/common/SelectBoxComponent";
+import buttonComponent from "@/components/common/ButtonComponent";
+import subInfoTitle from "@/components/common/SubInfoTitle";
+import labelComponent from "@/components/common/LabelComponent";
+import popupComponent from "@/components/common/PopupComponent";
+import HoliDiv from "@/components/common/AgGridComponent/HoliDiv";
+import HoliDesc from "@/components/common/AgGridComponent/HoliDesc";
+import ApiMixin from "@/service/common";
+
+export default {
+  mixins:[ApiMixin],
+  name: "HoliAdm",
+  components:{
+    agGridComponent,
+    labelComponent,
+    popupComponent,
+    datePickerComponent,
+    selectBoxComponent,
+    buttonComponent,
+    subInfoTitle,
+    HoliDiv,
+    HoliDesc
+  },
+  data(){
+    return{
+      holiRowData:[],
+      holiColumnDefs:[
+        {headerName:"일자" , field:"model1",
+          cellStyle:function (params){
+            if(params.data.model2==='일요일' || params.data.model2==='토요일')
+              return {color: 'red'};
+          },
+          width:100
+        },
+        {headerName:"요일" , field:"model2",
+          cellStyle:function (params){
+            if(params.data.model2==='일요일' || params.data.model2==='토요일')
+              return {color: 'red'};
+          },
+          width: 80
+        },
+        {headerName:"휴일 구분" , field:"model3",
+          cellRenderer :'HoliDiv',
+          cellRendererParams: {   //휴일 선택에 대한 반환값
+            clicked: function (field) {
+              console.log(field);
+            }
+          },
+          width: 220
+        },
+        {headerName:"휴일 설명" , field:"model4",
+          cellRenderer :'HoliDesc',
+          cellRendererParams: {   //휴일입력 대한 반환값
+            clicked: function (field) {
+              console.log(field);
+            }
+          },
+          width: 200
+        },
+      ],
+      holiYn:null,        //휴일 여부
+      holiDiv:null,      //휴일 구분
+      holiStDate:null,   //조회시작년월
+      holiEnDate:null,   //조회종료년월
+      isModalChgShow:false, //변경팝업
+      isModalCretShow:false //생성팝업
+    }
+  },
+  methods:{
+    reset(){
+      this.holiYn="";
+      this.holiDiv="";
+      this.holiStDate=new Date();
+      this.holiEnDate=new Date();
+    },
+    search(){
+      this.$connect('application/json','/info','get','').then((res)=>{
+        this.holiRowData = res.data.holiRowData;
+      })
+    }
+  },
+  async beforeMount() {
+    await this.$connect('application/json','/info','get','').then((res)=>{
+        this.holiRowData = res.data.holiRowData;
+    })
+  }
+}
+</script>
+
+<style scoped>
+.HoliAdmView{
+  display: grid;
+  grid-template-columns: 790px minmax(790px,1fr);
+  grid-template-rows: 100px minmax(640px, 1fr);
+  gap: 20px 20px;
+}
+
+.HoliAdmView > .item:nth-child(1) {
+  grid-column: 1/3;
+  grid-row: 1;
+}
+.HoliAdmView > .item:nth-child(2) {
+  grid-column: 1;
+  grid-row: 2;
+}
+.HoliAdmView > .item:nth-child(3) {
+  grid-column: 2;
+  grid-row: 2;
+}
+.holiSearch {
+  width: 100%;
+  padding-left: 10px;
+  background-color: rgb(239, 245, 252);
+  height: 100%;
+}
+table {
+  padding: 10px;
+}
+table > tr {
+  height: 35px;
+}
+table > tr > th {
+  margin-left: 10px;
+  width: 60px;
+  text-align: center;
+}
+.emptyBox1 {
+  width: 370px;
+}
+.emptyBox2 {
+  width: 370px;
+}
+.btnBox {
+  width: 80px;
+}
+.ag-grid_sp{
+  /* margin: 10px; */
+  width: 100%;
+  height: 600px;
+}
+.cretTable{
+  padding :0
+}
+.cretTable > tr{
+  height: 35px;
+}
+.cretTable > tr > th{
+  padding-left: 10px;
+  width: 200px;
+  text-align: left;
+  background-color: rgb(239 245 252);
+}
+.cretTable > tr > td {
+  justify-content: center;
+}
+</style>
