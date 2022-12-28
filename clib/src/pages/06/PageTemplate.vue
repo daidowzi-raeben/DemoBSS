e4e412f9acb012b3bd4cd37fc92ddbe30bae5c61
heonsooo <gjstn0308@naver.com>
2022-12-28 17:53:41 +0900

    Add Grid Template
    화면 생성 시 참고할 그리드 템플릿

--- clib/src/pages/06/PageTemplate.vue
+++ clib/src/pages/06/PageTemplate.vue
@@ -2,10 +2,10 @@
 <div>
   <p> grid 화면 템플릿 입니다.</p>
 
-  <article class="thisContainer">
-    <div class="item1"> a</div>
-    <div class="item2"> a</div>
-    <div class="item3"> a</div>
+  <article class="gridContainer">
+    <div class="item1"> 1번 div</div>
+    <div class="item2"> 2번 div</div>
+    <div class="item3"> 3번 div</div>
   </article>
 
 </div>
@@ -20,24 +20,28 @@ export default {
 </script>
 
 <style>
-.thisContainer {
+.gridContainer {
   display: grid;
   grid-template-columns: 850px 700px 1fr;
   grid-template-rows: 100px 640px 1fr;
   gap: 20px 30px;
 }
 
-.thisContainer > .item1 {
+.gridContainer > .item1 {
   grid-row: 1;
   grid-column: 1/3;
-  background-color: burlywood;
+  background-color: wheat;
 }
-.thisContainer > .item2 {
+.gridContainer > .item2 {
   grid-row: 2;
   grid-column: 1;
+  
+  background-color: antiquewhite;
 }
-.thisContainer > .item3 {
+.gridContainer > .item3 {
   grid-row: 2;
   grid-column: 2;
+  background-color: beige;
+
 }
 </style>
