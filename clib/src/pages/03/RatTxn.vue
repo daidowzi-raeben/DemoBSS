6eca87a3d13794ec7bae3e17c01c2fa453fb0607
heonsooo <gjstn0308@naver.com>
2023-01-03 14:53:09 +0900

    Add demoPage

---
+++ clib/src/pages/03/RatTxn.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>과금내역 테스트</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ clib/src/pages/04/CustLst.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+
+  
+    <article class="gridContainer">
+      <div class="item1"> 1번 div
+        <p>고객리스트 test Page</p>
+      </div>
+      <div class="item2"> 2번 div</div>
+      <div class="item3"> 3번 div</div>
+    </article>
+  
+  </div>
+  
+  
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  .gridContainer {
+    display: grid;
+    grid-template-columns: 850px 700px 1fr;
+    grid-template-rows: 100px 640px 1fr;
+    gap: 20px 30px;
+  }
+  
+  .gridContainer > .item1 {
+    grid-row: 1;
+    grid-column: 1/3;
+    background-color: wheat;
+  }
+  .gridContainer > .item2 {
+    grid-row: 2;
+    grid-column: 1;
+    
+    background-color: antiquewhite;
+  }
+  .gridContainer > .item3 {
+    grid-row: 2;
+    grid-column: 2;
+    background-color: beige;
+  
+  }
+  </style>
